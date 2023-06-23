639001d2bec6e3a16e54d3f36213f389e0225552
mahmoud94ha <mahmoudhannani3@gmail.com>
2023-06-23 04:53:27 +0100

    add

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -5,7 +5,6 @@ sudo apt-get update && sudo apt-get install -y make git
 #download hugo
 sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
 sudo apt install ./hugo.deb
-sudo curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.53.3
 #clean
 sudo rm hugo.deb
 
@@ -13,4 +12,3 @@ sudo rm hugo.deb
 sudo go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
 sudo npm install -g markdownlint-cli
 sudo npm install -g markdown-link-check
-
